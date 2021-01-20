c4ccd7e45a0072fc94964446882bc4eb3d556ddb
Mukesh <Mukeshkmr599@gmail.com>
2021-01-21 04:01:23 +0530

    OfficeCommit

--- 100 days of code/Excelconverter.py
+++ 100 days of code/Excelconverter.py
@@ -1,30 +1,51 @@
-import csv
+import csv,os
 
 clients = []
 client_server = {}
 servers_client = []
+def extarct_client(inputfilename):
+    """ This function will extract client and return back text file/s for individual patch group for a client"""
+    global client_server
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
+    filepath =(os.path.join(desktop,"SCM_Patching_Grouping"))
+    if os.path.exists(filepath) :
+        pass
+    else:
+        os.makedirs(filepath)
+    with open(inputfilename, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-with open("new 3.txt", "r") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    print(csv_reader)
-    for lines in csv_reader:
-        client = lines[1]
-        server_patch_group = lines[0]
-        if client not in clients:
-            clients.append(client)
-    for client in clients:
-        client_server[client] = []
-
-
-for c_clients in clients:
-    print(f'this is {c_clients}')
-    with open("new 3.txt", "r") as csv_ffile :
-        for lines in csv_ffile :
-            extracted_server = lines[0]
-            extracted_client = lines[1]
+        for lines in csv_reader:
+            client = lines[1]
             
-            if c_clients == extracted_client :
-                client_server[extracted_client].append(extracted_server)
-                print(extracted_server , extracted_client)
+            if client not in clients:
+                clients.append(client)
+        for client in clients:
+            client_server[client] = []
 
-print(client_server)
+        csv_file.seek(0)
+        for rows in csv_reader:
+            extracted_server = rows[0]
+            extracted_client = rows[1]
+
+            if extracted_client in client_server:
+
+                client_server[extracted_client].append(extracted_server)
+
+
+    client_server = dict(sorted(client_server.items()))
+    print("\n\n")
+    for key, values in client_server.items():
+        
+        clientfilename = (os.path.join(desktop,"SCM_Patching_Grouping",(f"{str(key)}.txt")))
+        
+        with open(clientfilename, 'w+') as newfile:
+            for n in values:
+                newfile.write(f'{n} \n')
+                
+        print(f"{key} has these {len(values)} \n")
+
+
+extarct_client("new 3.txt") #provide your inputfile in quotes
+
+
